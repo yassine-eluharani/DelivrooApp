b618215b869c06351a38cf94d4c719665155bc41
Yassine Eluharani <eluharani.yassine@gmail.com>
2022-10-26 14:35:33 +0000

    Switch to typescript

--- App.tsx
+++ App.tsx
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>initialzing git</Text>
+      <Text>initializing git</Text>
       <StatusBar style="auto" />
     </View>
   );
